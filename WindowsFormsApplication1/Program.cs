c12985349a1949e1a678d870bc7269072b7eb078
yangzhou <jerry0507@sohu.com>
2019-02-17 10:28:00 +0800

    yz提交

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
+            string aa = "bb";
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
